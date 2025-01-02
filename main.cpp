452e370c71d5aef016c64dcfaca69edd09e7488c
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-01-02 14:32:51 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,57 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+string v[5], w[5];
+float x[10], y[10];
+int i, p, d;
+i=0;
+while (i<5)
+{
+    v[0]="amy";
+    v[1]="gino";
+    v[2]="Pino";
+    v[3]="anna";
+    v[4]="Lena";
+    cout << "Vettore V cella numero " << i+1 << ":" << v[i] << endl;
+    i=i+1;
+
+}
+i=0;
+while (i<5)
+{
+    w[0]="Antonio";
+    w[1]="Greta";
+    w[2]="Giulio";
+    w[3]="Giorgio";
+    w[4]="Giulia";
+    cout << "Vettore W cella numero " << i+1 << ":" << w[i] << endl;
+    i=i+1;
+}
+i=1;
+while(i<10)
+{
+    x[i]=i;
+    if( x[i]%2=0)
+    {
+        p=x[i];
+        cout << "Vettore X con indice pari cella numero " << i << ":" <<  p << endl;
+        cout << "Vettore X con valore pari cella numero " << i << ":" <<  p << endl;
+    }
+    else
+    {
+        d=x[i];
+        cout << "Vettore X con indice dispari cella numero " << i << ":" <<  d;
+    }
+    i=i+1;
+
+}
+i=0;
+while(i*2<=10)
+{
+    y[i]=i*2;
+    cout << "Vettore Y cella numero " << i+1 << ":" << y[i] << endl;
+    i=i+1;
+}
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
